1932e0cb103cbdd01f932319cfcbf2ea4f802cf8
David Durst <davidbdurst@gmail.com>
2021-01-02 20:30:20 -0800

    added add and map2

--- lib/Dialect/SequenceLanguage/Ops.cpp
+++ lib/Dialect/SequenceLanguage/Ops.cpp
@@ -35,7 +35,50 @@ static LogicalResult verifyMapTypesMatch(MapOp *op) {
 
     if (in_t.getN() != out_t.getN()) {
         return op->emitError("Map input length ")
-            << out_t.getN()
+            << in_t.getN()
+            << " doesn't match output length "
+            << out_t.getN();
+    }
+
+    return success();
+}
+
+static LogicalResult verifyMap2TypesMatch(Map2Op *op) {
+    FunctionType f_t = op->f().getType().cast<FunctionType>();
+    SeqType in0_t = op->in0().getType().cast<SeqType>();
+    SeqType in1_t = op->in1().getType().cast<SeqType>();
+    if (f_t.getInput(0) != in0_t.getT()) {
+        return op->emitError("Map f input 0 type ")
+            << f_t.getInput(0)
+            << " doesn't match element of input seq type"
+            << in0_t;
+    }
+
+    if (f_t.getInput(1) != in1_t.getT()) {
+        return op->emitError("Map f input 1 type ")
+            << f_t.getInput(1)
+            << " doesn't match element of input seq type"
+            << in0_t;
+    }
+
+    SeqType out_t = op->out().getType().cast<SeqType>();
+    if (f_t.getResult(0) != out_t.getT()) {
+        return op->emitError("Map f output type ")
+            << f_t.getResult(0)
+            << " doesn't match element of output seq type"
+            << out_t;
+    }
+
+    if (in0_t.getN() != out_t.getN()) {
+        return op->emitError("Map in 0 length ")
+            << in0_t.getN()
+            << " doesn't match output length "
+            << out_t.getN();
+    }
+
+    if (in1_t.getN() != out_t.getN()) {
+        return op->emitError("Map in 0 length ")
+            << in1_t.getN()
             << " doesn't match output length "
             << out_t.getN();
     }
